97e06aca06138d7acec4f8205f1283e5ed2a1b2a
Elizabeth A Kari <eakari@live.com>
2013-09-23 21:07:15 -0700

    fixed errors

--- app/models/license.rb
+++ app/models/license.rb
@@ -1,10 +1,10 @@
 class License < ActiveRecord::Base
   belongs_to :user
 
-  validates :state, format: { with: /\A[A-]{2}\z/ }
+  validates :state, format: { with: /\A[A-Z]{2}\z/ },
     length: { is: 2 }
 
-  def state+(value)
+  def state=(value)
     value = value.strip.upcase
     write_attribute :state, value
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130921213727) do
+ActiveRecord::Schema.define(version: 20130924035334) do
 
   create_table "licenses", force: true do |t|
     t.string   "number"
     t.string   "state"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
